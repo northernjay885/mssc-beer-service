119819336a41810fa11b0636c714493d2b9bd1f3
luorenjie <lrj193927@gmail.com>
2021-01-11 10:12:09 -0500

    add ehcache to BeerPagedList and getbyId. Closes #19

---
+++ src/main/java/org/northernjay/msscbeerservice/config/CacheConfig.java
@@ -0,0 +1,11 @@
+package org.northernjay.msscbeerservice.config;
+
+
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.context.annotation.Configuration;
+
+@EnableCaching
+@Configuration
+public class CacheConfig {
+
+}
--- src/main/java/org/northernjay/msscbeerservice/services/BeerServiceImpl.java
+++ src/main/java/org/northernjay/msscbeerservice/services/BeerServiceImpl.java
@@ -9,6 +9,7 @@ import org.northernjay.msscbeerservice.web.mappers.BeerMapper;
 import org.northernjay.msscbeerservice.web.model.BeerDto;
 import org.northernjay.msscbeerservice.web.model.BeerPagedList;
 import org.northernjay.msscbeerservice.web.model.BeerStyleEnum;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -23,6 +24,7 @@ public class BeerServiceImpl implements BeerService {
     private final BeerRepository beerRepository;
     private final BeerMapper beerMapper;
 
+    @Cacheable(cacheNames = "beerCache", key = "#beerId", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerDto getById(UUID beerId, boolean showInventoryOnHand) {
         if (showInventoryOnHand) {
@@ -51,11 +53,14 @@ public class BeerServiceImpl implements BeerService {
         return beerMapper.beerToBeerDto(beerRepository.save(beerToUpdate));
     }
 
+    @Cacheable(cacheNames = "beerListCache", condition = "#showInventoryOnHand == false ")
     @Override
     public BeerPagedList listBeers(String beerName, BeerStyleEnum beerStyle, boolean showInventoryOnHand, PageRequest pageRequest) {
 
         BeerPagedList beerPagedList;
         Page<Beer> beerPage;
+
+        System.out.println("I was called");
 
         if (!StringUtils.isEmpty(beerName) && beerStyle != null) {
             //search both
