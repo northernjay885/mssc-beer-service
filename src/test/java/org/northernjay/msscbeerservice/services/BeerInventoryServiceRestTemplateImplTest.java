adb11e361145a5fb285ee48458465f3b8c81bd02
luorenjie <lrj193927@gmail.com>
2021-01-12 17:22:32 -0500

    init data through bootstrap. Closes #24

--- src/test/java/org/northernjay/msscbeerservice/services/BeerInventoryServiceRestTemplateImplTest.java
+++ src/test/java/org/northernjay/msscbeerservice/services/BeerInventoryServiceRestTemplateImplTest.java
@@ -3,7 +3,6 @@ package org.northernjay.msscbeerservice.services;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
-import org.northernjay.msscbeerservice.bootstraps.BeerLoader;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -20,8 +19,9 @@ class BeerInventoryServiceRestTemplateImplTest {
 
     @Test
     void getOnHandInventory() {
-        Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
+        //todo evolve to use UPC
+        //Integer qoh = beerInventoryService.getOnHandInventory(BeerLoader.BEER_1_UUID);
 
-        System.out.println(qoh);
+        //System.out.println(qoh);
     }
 }
